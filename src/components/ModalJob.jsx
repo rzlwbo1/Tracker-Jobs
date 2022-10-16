a622f722e1a900d1f2fbbaa36f40ce7fb36d77bd
rzlwbo1 <rizalwb2@gmail.com>
2022-10-16 14:12:18 +0700

    all uncrontroller comp

--- src/components/ModalJob.jsx
+++ src/components/ModalJob.jsx
@@ -2,69 +2,93 @@ import React from "react";
 import Modal from "react-bootstrap/Modal";
 import Button from "react-bootstrap/Button";
 
-export default function ModalJob({ show, handleClose, editJob }) {
+class ModalJob extends React.Component {
 
-  function handleSubmitEdit(e) {
-    console.log()
+  constructor(props) {
+    super(props);
+
+    
+    this.handleSubmitEdit = this.handleSubmitEdit.bind(this)
+    this.dateInput = React.createRef() 
+    this.roleInput = React.createRef() 
+    this.companyInput = React.createRef() 
+    this.statusInput = React.createRef() 
   }
 
-  return (
-    <>
-      <Modal show={show} onHide={handleClose}>
-        <Modal.Header closeButton>
-          <Modal.Title>Editing Jobs</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          <form onSubmit={handleSubmitEdit}>
-            <div className="mb-3">
-              <label className="form-label">Date Applied</label>
-              <input
-                type="date"
-                name="date"
-                className="form-control"
-                defaultValue={editJob.date}
-              />
-            </div>
+  handleSubmitEdit(ev) {
+    ev.preventDefault();
 
-            <div className="mb-3">
-              <label className="form-label">Role</label>
-              <input
-                type="text"
-                name="role"
-                className="form-control"
-                defaultValue={editJob.role}
-              />
-            </div>
+    // console.log(this.dateInput.current.value);
+  }
+  
 
-            <div className="mb-3">
-              <label className="form-label">Company</label>
-              <input
-                type="text"
-                name="company"
-                className="form-control"
-                defaultValue={editJob.company}
-              />
-            </div>
+  render() {
+    return (
+      <>
+        <Modal show={this.props.show} onHide={this.props.handleClose}>
+          <Modal.Header closeButton>
+            <Modal.Title>Editing Jobs</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <form onSubmit={this.handleSubmitEdit}>
+              <div className="mb-3">
+                <label className="form-label">Date Applied</label>
+                <input
+                  type="date"
+                  name="date"
+                  className="form-control"
+                  id="editDate"
+                  defaultValue={this.props.editJob.date}
+                  ref={this.dateInput}
+                />
+              </div>
+  
+              <div className="mb-3">
+                <label className="form-label">Role</label>
+                <input
+                  type="text"
+                  name="role"
+                  className="form-control"
+                  defaultValue={this.props.editJob.role}
+                  ref={this.roleInput}
+                />
+              </div>
+  
+              <div className="mb-3">
+                <label className="form-label">Company</label>
+                <input
+                  type="text"
+                  name="company"
+                  className="form-control"
+                  defaultValue={this.props.editJob.company}
+                  ref={this.companyInput}
+                />
+              </div>
+  
+              <div className="mb-3">
+                <label className="form-label">Status</label>
+                <select className="form-select" aria-label="Default select example" defaultValue={this.props.editJob.status} ref={this.statusInput}>
+                  <option value="accepted">accepted</option>
+                  <option value="applied">applied</option>
+                  <option value="rejected">rejected</option>
+                  <option value="test">test</option>
+                  <option value="ghosting">ghosting</option>
+                  <option value="interview">interview</option>
+                </select>
+              </div>
 
-            <div className="mb-3">
-              <label className="form-label">Status</label>
-              <select className="form-select" aria-label="Default select example" defaultValue={editJob.status}>
-                <option value="accepted">accepted</option>
-                <option value="applied">applied</option>
-                <option value="rejected">rejected</option>
-                <option value="test">test</option>
-                <option value="ghosting">ghosting</option>
-                <option value="interview">interview</option>
-              </select>
-            </div>
-          </form>
-        </Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={handleClose}>
-            Close
-          </Button>
-        </Modal.Footer>
-      </Modal>
-    </>
-  );
+              <button type="submit" className="btn btn-primary" onClick={this.props.handleClose}>Save</button>
+            </form>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button variant="secondary" onClick={this.props.handleClose}>
+              Close
+            </Button>
+          </Modal.Footer>
+        </Modal>
+      </>
+    );
+  }
 }
+
+export default ModalJob;
